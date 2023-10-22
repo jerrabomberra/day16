670d3147642e1a1b8626d030cf167f485d9fa3a2
Doug Gordon <gordononline@bigpond.com>
2023-10-23 06:37:07 +1100

    final

--- money_machine.py
+++ money_machine.py
@@ -1,34 +1,46 @@
 """ money machine class"""
 
-class CoffeeMaker:
-    """Models the machine that makes the coffee"""
+class MoneyMachine:
+
+    CURRENCY = "$"
+
+    COIN_VALUES = {
+        "quarters": 0.25,
+        "dimes": 0.10,
+        "nickles": 0.05,
+        "pennies": 0.01
+    }
+
+
     def __init__(self):
-        self.resources = {
-            "water": 300,
-            "milk": 200,
-            "coffee": 100,
-        }
+        self.profit = 0
+        self.money_received = 0
 
 
     def report(self):
-        """Prints a report of all resources."""
-        print(f"Water: {self.resources['water']}ml")
-        print(f"Milk: {self.resources['milk']}ml")
-        print(f"Coffee: {self.resources['coffee']}g")
+        """Prints the current profit"""
+        print(f"Money: {self.CURRENCY}{self.profit}")
 
 
-    def is_resource_sufficient(self, drink):
-        """Returns True when order can be made, False if ingredients are insufficient."""
-        can_make = True
-        for item in drink.ingredients:
-            if drink.ingredients[item] > self.resources[item]:
-                print(f"Sorry there is not enough {item}.")
-                can_make = False
-        return can_make
+    def process_coins(self):
+        """Returns the total calculated from coins inserted."""
+        print("Please insert coins.")
+        for coin in self.COIN_VALUES:
+            self.money_received += int(input(f"How many {coin}?: ")) * self.COIN_VALUES[coin]
+        return self.money_received
 
 
-    def make_coffee(self, order):
-        """Deducts the required ingredients from the resources."""
-        for item in order.ingredients:
-            self.resources[item] -= order.ingredients[item]
-        print(f"Here is your {order.name} ☕️. Enjoy!")
+    def make_payment(self, cost):
+        """Returns True when payment is accepted, or False if insufficient."""
+        self.process_coins()
+        if self.money_received >= cost:
+            change = round(self.money_received - cost, 2)
+            print(f"Here is {self.CURRENCY}{change} in change.")
+            self.profit += cost
+            self.money_received = 0
+            return True
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+            self.money_received = 0
+            return False
+        
